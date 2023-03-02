85307f81f4408eb61cd17cfe39bf790553b300b7
IndianBartonka <bartonka2017@outlook.com>
2023-03-02 14:44:00 +0100

    Prefixes util interface created

---
+++ src/main/java/me/indian/ostag/utils/PrefixesUtil.java
@@ -0,0 +1,26 @@
+package me.indian.ostag.utils;
+
+public interface PrefixesUtil {
+    String NAME = "<name>";
+    String PING = "<ping>";
+    String HEALTH = "<health>";
+    String MAX_HEALTH = "<max_health>";
+    String FOOD = "<food>";
+    String MAX_FOOD = "<max_food>";
+    String DIMENSION = "<dimension>";
+    String XP = "<xp>";
+    String GAMEMODE = "<gamemode>";
+    String MSG = "<msg>";
+    String PREFFIX = "<preffix>";
+    String SUFFIX = "<suffix>";
+    String GROUPDISPLAYNAME = "<groupDisName>";
+    String MODEL = "<model>";
+    String DEVICE = "<device>";
+    String CONTROLLER = "<controller>";
+    String VERSION = "<version>";
+    String XUID = "<xuid>";
+    String LANGUAGE = "<language>";
+    String CPS = "<cps>";
+    String UNIQUE_DESCRIPTION = "<unique_description>";
+
+}
