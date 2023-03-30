c901dba68a0493cd3348e4b9af02b7448f16779a
IndianBartonka <bartonka2017@outlook.com>
2023-03-30 14:03:48 +0200

    Better code style (optymalization fix)

--- src/main/java/me/indian/ostag/listeners/InputListener.java
+++ src/main/java/me/indian/ostag/listeners/InputListener.java
@@ -25,13 +25,13 @@ public class InputListener implements Listener {
             final Player player = event.getPlayer();
             //thanks to Petterim
             //https://github.com/PetteriM1
-          if(controller.containsKey(player)){
-      controller.put(player, inputMode);
-      return;
-}
-if(controller.get(player) != inputMode)}
-controller.put(player, inputMode);
-} 
+            if (!controller.containsKey(player)) {
+                controller.put(player, inputMode);
+                return;
+            }
+            if (controller.get(player) != inputMode) {
+                controller.put(player, inputMode);
+            }
         }
     }
 
