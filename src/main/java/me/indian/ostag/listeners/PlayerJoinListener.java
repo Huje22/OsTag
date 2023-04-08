2afe2a646da4f611f396e732b096a15d47c8be6f
IndianBartonka <bartonka2017@outlook.com>
2023-04-08 20:59:13 +0200

    fix small code style

--- src/main/java/me/indian/ostag/listeners/PlayerJoinListener.java
+++ src/main/java/me/indian/ostag/listeners/PlayerJoinListener.java
@@ -27,7 +27,7 @@ public class PlayerJoinListener implements Listener {
 
         if (player.hasPermission(Permissions.ADMIN) || player.isOp()) {
             if(config.getBoolean("UpdateChecker")) {
-                player.sendMessage(OsTag.prefix + " " + compatibility);
+                player.sendMessage(plugin.prefix + " " + compatibility);
             }
         }
     }
