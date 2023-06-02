35b5c9ead8f09a6b84d3348e72679e547039c0ee
IndianBartonka <bartonka2017@outlook.com>
2023-06-02 14:32:44 +0200

    Make ThreadUtil for changing threads names

---
+++ src/main/java/me/indian/ostag/util/ThreadUtil.java
@@ -0,0 +1,18 @@
+package me.indian.ostag.util;
+
+import java.util.concurrent.ThreadFactory;
+
+public class ThreadUtil implements ThreadFactory {
+    private final String threadName;
+
+    public ThreadUtil(String threadName) {
+        this.threadName = threadName;
+    }
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        Thread thread = new Thread(runnable);
+        thread.setName(threadName);
+        return thread;
+    }
+}
