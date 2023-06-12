0f98dd245596e26b00e2d04c22fae58de48c98ea
IndianBartonka <bartonka2017@outlook.com>
2023-06-12 10:53:22 +0200

    new methods

--- src/main/java/me/indian/ostag/util/MessageUtil.java
+++ src/main/java/me/indian/ostag/util/MessageUtil.java
@@ -3,10 +3,13 @@ package me.indian.ostag.util;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.utils.TextFormat;
+import me.indian.ostag.OsTag;
 
 import java.util.List;
 
 public class MessageUtil {
+
+    private static final Server server = Server.getInstance();
 
     public static String colorize(final String msg) {
         return TextFormat.colorize('&', msg);
@@ -19,8 +22,23 @@ public class MessageUtil {
         return String.join("\n", lista);
     }
 
+    public static String listToString(List<String> lista) {
+        if (lista == null) {
+            return "";
+        }
+        return String.join(" ", lista);
+    }
+
+    public static void playerCommand(final Player player, final String command) {
+        server.dispatchCommand(player, command);
+    }
+
+    public static void consoleCommand(final String command) {
+        server.dispatchCommand(server.getConsoleSender(), command);
+    }
+
     public static void sendMessageToAll(final String msg) {
-        for (final Player all : Server.getInstance().getOnlinePlayers().values()) {
+        for (final Player all : server.getOnlinePlayers().values()) {
             all.sendMessage(msg);
         }
     }
