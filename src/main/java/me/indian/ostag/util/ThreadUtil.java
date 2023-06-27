e4f3ab2a5f76caf6bd127d36621f2ed2f90c1ea2
IndianBartonka <bartonka2017@outlook.com>
2023-06-27 14:47:22 +0200

    Update ThreadUtil.java

--- src/main/java/me/indian/ostag/util/ThreadUtil.java
+++ src/main/java/me/indian/ostag/util/ThreadUtil.java
@@ -4,13 +4,27 @@ import java.util.concurrent.ThreadFactory;
 
 public class ThreadUtil implements ThreadFactory {
     private final String threadName;
+    private final Runnable runnable;
 
-    public ThreadUtil(String threadName) {
+
+    public ThreadUtil(final String threadName) {
         this.threadName = threadName;
+        this.runnable = null;
+    }
+
+    public ThreadUtil(final String threadName, final Runnable runnable) {
+        this.threadName = threadName;
+        this.runnable = runnable;
     }
 
     @Override
-    public Thread newThread(Runnable runnable) {
+    public Thread newThread(final Runnable runnable) {
+        Thread thread = new Thread(runnable);
+        thread.setName(threadName);
+        return thread;
+    }
+
+    public Thread newThread() {
         Thread thread = new Thread(runnable);
         thread.setName(threadName);
         return thread;
